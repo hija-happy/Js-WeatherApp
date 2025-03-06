ad3aaaa3388923339defe804fe5088080711efc9
hija-happy <hijahappy2004@gmail.com>
2025-03-06 21:28:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const apiKey = "9f1d272b08a2e83973f07060314f0c12";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=bangalore";
+
+
+async function checkWeather() {
+    const response =await fetch()
+}
